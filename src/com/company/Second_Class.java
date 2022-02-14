1b340edba5576c294bca709ca91ae5302219bd9a
Unal <unalakcay1034@gmail.com>
2022-02-13 18:04:35 -0700

    My new class1

--- src/com/company/Second_Class.java
+++ src/com/company/Second_Class.java
@@ -3,5 +3,6 @@ package com.company;
 public class Second_Class {
     public static void main(String[] args) {
         System.out.println("new page second class");
+        System.out.println("how are you");
     }
 }
